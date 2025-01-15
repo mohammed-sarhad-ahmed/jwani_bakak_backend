a3fd9ff959b3d38aabd65c7c17075f4cde75b6c8
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-01-15 23:13:33 +0300

    finish company route

--- controller/companycontroller.js
+++ controller/companycontroller.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { addCompany } from "./../handler/companyhandler.js";
+import {
+  addCompany,
+  getCompanies,
+  getCompany,
+  updateCompany,
+  deleteCompany,
+} from "./../handler/companyhandler.js";
 
 const router = express.Router();
-router.route("/").post(addCompany);
+router.route("/").post(addCompany).get(getCompanies);
+router.route("/:id").get(getCompany).patch(updateCompany).delete(deleteCompany);
 
 export default router;
--- handler/companyhandler.js
+++ handler/companyhandler.js
@@ -21,3 +21,67 @@ export async function addCompany(req, res) {
     });
   }
 }
+
+export async function getCompanies(req, res) {
+  try {
+    const products = await CompanyModel.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        products,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+}
+
+export async function getCompany(req, res) {
+  try {
+    const companyId = req.params.id;
+    const product = await CompanyModel.findById(companyId);
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+}
+
+export async function deleteCompany(req, res) {
+  try {
+    const companyId = req.params.id;
+    const product = await CompanyModel.findByIdAndDelete(companyId);
+    res.status(204).end();
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+}
+
+export async function updateCompany(req, res) {
+  try {
+    const companyId = req.params.id;
+    const product = await CompanyModel.findByIdAndUpdate(companyId, req.body, {
+      runValidators: true,
+      new: true,
+    });
+    res.status(204).json(product);
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+}
