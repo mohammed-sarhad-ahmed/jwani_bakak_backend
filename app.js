e35ad9a7da05088cecc6ffb862083d253207049e
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-01-15 23:44:13 +0300

    add 404 route

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use("/company", companyRouter);
-
+app.all("*", (req, res) => {
+  res.status(404).json({
+    message: `failed to find the route ${req.originalUrl}`,
+  });
+});
 const databasePort = process.env.DATABASEPORT;
 const databaseAddress = process.env.DATABASEADDRESS;
 console.log(databaseAddress, databasePort);
