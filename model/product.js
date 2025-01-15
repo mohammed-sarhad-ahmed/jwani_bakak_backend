33e3d39070dad6909e12107af54e87a5b781d969
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-01-15 15:15:55 +0300

    add timestamps for products

--- model/product.js
+++ model/product.js
@@ -1,14 +1,19 @@
 import mongoose, { Schema } from "mongoose";
 
-const productSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: [true, "the product must have a name"],
+const productSchema = new mongoose.Schema(
+  {
+    productName: {
+      type: String,
+      required: [true, "the product must have a name"],
+    },
+    brand: {
+      type: mongoose.Types.ObjectId,
+      required: [true, "the brand for the product must be specified"],
+    },
   },
-  brand: {
-    type: mongoose.Types.ObjectId,
-    required: [true, "the brand for the product must be specified"],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model("Product", productSchema);
