930b377a6d4f82cdc3c6a2d376b0249914e76a86
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-01-15 15:42:19 +0300

    finish brand model

--- model/brand.js
+++ model/brand.js
@@ -1,3 +1,17 @@
 import mongoose from "mongoose";
 
-const brandSchema = new mongoose.Schema({});
+const brandSchema = new mongoose.Schema(
+  {
+    brandName: {
+      type: String,
+      required: [true, "the brandName must be provided"],
+    },
+    logoPath: {
+      type: String,
+      required: [true, "you must provide a path to the brand logo"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
