15c7ec468945aa9ebd7a954fbeafbd985edeb946
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-01-16 13:36:47 +0300

    finish klesh controller

--- controller/kleshcontroller.js
+++ controller/kleshcontroller.js
@@ -1,7 +1,14 @@
 import express from "express";
-
+import {
+  addKlesh,
+  deleteKlesh,
+  getKlesh,
+  getKleshes,
+  updateKlesh,
+} from "../handler/kleshHandler";
 const router = express.Router();
 
-router.route("/").post();
+router.route("/").post(addKlesh).get(getKleshes);
+router.route("/:id").get(getKlesh).delete(deleteKlesh).patch(updateKlesh);
 
 export default router;
