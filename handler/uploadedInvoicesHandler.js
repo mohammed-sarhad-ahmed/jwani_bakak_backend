a4ea028e43ec1856637134c306f7d3b520219cee
baran6688 <baranali6688@gmail.com>
2025-02-19 21:57:45 +0300

    for root dir

--- handler/uploadedInvoicesHandler.js
+++ handler/uploadedInvoicesHandler.js
@@ -5,6 +5,8 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const rootDir = path.join(__dirname, "../"); // Moves up one level
+
 export async function uploadInvoice(req, res) {
   try {
     const { filename } = req.file;
@@ -51,7 +53,8 @@ export async function getUploadedInvoices(req, res) {
 export async function getUploadedInvoice(req, res) {
   try {
     const { id } = req.params;
-    const filePath = path.join(__dirname, "/public/uploadedInvoicesImg/", id);
+    const filePath = path.join(rootDir, "public/uploadedInvoicesImg/", id);
+
     res.setHeader(
       "Content-Disposition",
       "attachment; filename=downloaded-file.pdf",
