3f66c921e243512fe6da874a4299470158759d47
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-03-24 16:59:12 +0300

    finish update buy transaction

--- handler/handlebuytransaction.js
+++ handler/handlebuytransaction.js
@@ -86,6 +86,22 @@ export async function deleteBuyTransaction(req, res) {
 
 export async function updateBuyTransaction(req, res) {
   try {
+    const product = await ProductModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,
+        runValidators: true,
+      }
+    )
+      .populate("company")
+      .populate("ComposedProduct");
+    res.status(200).json({
+      message: "success",
+      data: {
+        product,
+      },
+    });
   } catch (error) {
     res.status(400).json({
       status: "fail",
