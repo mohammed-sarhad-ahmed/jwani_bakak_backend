dc260b60cbf3871e7f24cd2669b438a558d8a598
mohammed-sarhad-ahmed <120845827+mohammed-sarhad-ahmed@users.noreply.github.com>
2025-03-25 20:30:45 +0300

    Update exchange.js

--- handler/exchange.js
+++ handler/exchange.js
@@ -18,7 +18,7 @@ export async function getExchange(req, res) {
 }
 export async function getCurrent(req, res) {
   try {
-    const exchange = await exchangeModel.findOne().limit(1);
+    const exchange = await exchangeModel.findOne().sort("-createdAt");
     res.status(200).send({
       message: "success",
       data: {
